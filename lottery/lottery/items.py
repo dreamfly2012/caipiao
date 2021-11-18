de373df139c997f4f64aba0a38c2dfbc95c64cea
menghuiguli <fujia0@qq.com>
2021-11-18 21:08:03 +0800

    change

--- lottery/lottery/items.py
+++ lottery/lottery/items.py
@@ -9,4 +9,8 @@ import scrapy
 class LotteryItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    periods = scrapy.Field()
+    nums1 = scrapy.Field()
+    nums2 = scrapy.Field()
+    nums3 = scrapy.Field()
     pass
